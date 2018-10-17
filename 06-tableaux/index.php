e5aad8f790e131367e18604117dcd25f3bc69c97
guillaume-goubel <guillaume.goubel.pro@gmail.com>
2018-10-17 14:49:09 +0200

    correction _ 17_10_2018

--- 06-tableaux/index.php
+++ 06-tableaux/index.php
@@ -13,19 +13,19 @@
     $eleves = [
         0 => [
             'nom' => 'Matthieu',
-            'notes' => [10, 8, 16, 20, 17, 16, 15, 2]
+            'notes' => [10, 8, 16, 16, 17, 16, 19, 2]
         ],
         1 => [
             'nom' => 'Thomas',
-            'notes' => [4, 18, 12, 15, 13, 7]
+            'notes' => [4, 18, 12, 20, 13, 7]
         ],
         2 => [
             'nom' => 'Jean',
-            'notes' => [17, 14, 6, 2, 16, 18, 9]
+            'notes' => [17, 14, 16, 18, 9]
         ],
         3 => [
             'nom' => 'Enzo',
-            'notes' => [1, 14, 6, 2, 1, 8, 9]
+            'notes' => [1, 14, 12, 2, 1, 8, 9]
         ]
     ];
 
@@ -49,6 +49,7 @@ echo '<br>'; */
 
 
 // AFFICHER LES NOTES
+echo '<h2> 1.AFFICHER LES NOTES </h2> ';
  
  foreach ($eleves as $key1 => $value1) {
     
@@ -76,13 +77,15 @@ echo '<br>'; */
         }
     }
 
-    echo ('<br>');
 
 }
 
     // LA MOYENNE DE JEAN
+    echo '<h2> 2.LA MOYENNE DE JEAN </h2> ';
+
+
     $notesCount = count($eleves[2]['notes']) ; 
-    echo ('<br>');
+
 
     $somme = 0;
 
@@ -100,11 +103,11 @@ echo '<br>'; */
     echo (array_sum($eleves[2]['notes']) / count($eleves[2]['notes'])); // la moyenne avec des fonctions
 
     
-    echo ('<br>');
 
 
     
     //QUI A LA MOYENNE ?
+    echo '<h2> 3.QUI A LA MOYENNE </h2> ';
     $countAverage = 0;
     foreach ($eleves as $eleve) {
 
@@ -125,6 +128,8 @@ echo '<br>'; */
 
 
     // LA MEILLEURE NOTE
+    echo '<h2> 4.QUI A LA MEILLEURE NOTE </h2> ';
+    
     $bestnote = 0;
 
     foreach ($eleves as $eleve) {
@@ -138,19 +143,54 @@ echo '<br>'; */
         }
     }
 
-
     echo ('<br>');
 
     foreach ($eleves as $eleve) {
         foreach ($eleve['notes'] as $note) {
 
             if ($note ===  $bestnote){
-
-                echo $eleve['nom'] .'a eu la meilleur note qui est ' . $bestnote;
+                echo $eleve['nom'] .' a eu la meilleure note qui est ' . $bestnote;
+                break; // on arrete la boucle
             }
         
         }
     }
+
+
+        // QUELQU'UN A EU 20 ?
+        echo '<h2> 5.QUI A EU 20 ? </h2> ';
+
+        $vingt = false;
+
+        foreach ($eleves as $eleve){
+            if ($eleve['nom'] === 'Thomas'){
+                continue; // Si l'élève s'appel Thomas , on continue
+            }
+
+            foreach ($eleve['notes'] as $note) {
+                
+                if ($note ===  20){
+                    $vingt = true;
+                    break 2;
+                    var_dump('ici');           
+               }
+            }
+            //break
+            var_dump('là');   
+        }
+
+        if ($vingt){
+            echo "Quelqu'un a eu 20";
+        } else {
+            echo "Personne n'a eu 20 , bande de nazes !";
+        }
+
+    
+
+
+
+
+
 
 
 
